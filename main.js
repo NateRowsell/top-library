1efbb6919dbeb2bad86a62d194c7e5c5be1cf949
NateRowsell <nsr.72@hotmail.com>
2022-02-22 20:45:41 -0400

    finished add book form

--- main.js
+++ main.js
@@ -1,28 +1,57 @@
-let userLibrary = []
+const submitNewBookButton = document.getElementById('submitNewBook')
+const form = document.getElementById('addBook')
 
-function Book(title, author, pages) {
+let userLibrary = []
+let title = ''
+let author = ''
+let isRead = false
+let pages = 0
+
+function Book(title, author, pages, isRead) {
   this.title = title
   this.author = author
   this.pages = pages
+  this.isRead = isRead
   this.addBook = function () {
     userLibrary.push(this)
   }
 }
 
-function addNewBook(title, author, pages) {
-  let newBook = new Book(title, author, pages)
+function addNewBook(title, author, pages, isRead) {
+  let newBook = new Book(title, author, pages, isRead)
   newBook.addBook()
 }
 
-addNewBook('harry potter', 'tolkein', '380')
-addNewBook('lord of the rings', 'jr rowling', '600')
-addNewBook('game of thrones', 'george r r martin', '980')
-
 console.log(userLibrary)
 
-function openForm() {
-  document.getElementById('popupForm').style.display = 'block'
+// CODE FOR POP UP FORM START
+
+let modalBtns = [...document.querySelectorAll('.button')]
+modalBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    let modal = btn.getAttribute('data-modal')
+    document.getElementById(modal).style.display = 'block'
+  }
+})
+let closeBtns = [...document.querySelectorAll('.close')]
+closeBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    let modal = btn.closest('.modal')
+    modal.style.display = 'none'
+  }
+})
+window.onclick = function (event) {
+  if (event.target.className === 'modal') {
+    event.target.style.display = 'none'
+  }
 }
-function closeForm() {
-  document.getElementById('popupForm').style.display = 'none'
-}
+
+// CODE FOR POP UP FORM END
+
+submitNewBookButton.addEventListener('click', () => {
+  title = form.title.value
+  author = form.title.value
+  pages = form.title.value
+  isRead = form.isRead.checked
+  addNewBook(title, author, pages, isRead)
+})
